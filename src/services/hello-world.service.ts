be5274a54e39da6e6bf2493d56aaa8ee7d5b0076
Alex Au <nh47361@hertz.net>
2019-11-19 16:50:27 -0800

    added logDNA message

--- src/services/hello-world.service.ts
+++ src/services/hello-world.service.ts
@@ -1,6 +1,6 @@
-import {HelloWorldApi} from './hello-world.api';
-import {Inject, Provides, Singleton} from 'typescript-ioc';
-import {LoggerApi} from '../logger';
+import { HelloWorldApi } from './hello-world.api';
+import { Inject, Provides, Singleton } from 'typescript-ioc';
+import { LoggerApi } from '../logger';
 
 @Singleton
 @Provides(HelloWorldApi)
@@ -9,13 +9,25 @@ export class HelloWorldService implements HelloWorldApi {
 
   constructor(
     @Inject
-    logger: LoggerApi,
+    logger: LoggerApi
   ) {
     this.logger = logger.child('HelloWorldService');
   }
 
   async greeting(name: string = 'World'): Promise<string> {
     this.logger.info(`Generating greeting for ${name}`);
+    this.logger.debug('This is a debug message from inside my API');
+    // Only Trigger error if the name passed in matches ERROR
+    if (name === 'ERROR') {
+      try {
+        throw new Error('"Throwing an error');
+      } catch (e) {
+        this.logger.error(e);
+        throw new Error('"Catching and Throwing errors');
+      } finally {
+        throw new Error('Last chance to trap this Error');
+      }
+    }
     return `Hello, ${name}!`;
   }
 }
